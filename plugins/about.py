1175d996ecbca1e5368f14c5b26366594999385e
Mr Noob Developer <76585579+fazinek@users.noreply.github.com>
2021-06-15 20:11:28 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -4,14 +4,6 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["about"]))
 async def start(client, message):
 
-    about = """
-- **Bot :** `Rename Bot`
-- **Creator :** [Fayas](https://telegram.me/TheFayas)
-- **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
-- **Credits :** `Everyone in this journey`
-- **Source :** [Click here](https://github.com/FayasNoushad/Rename-Bot)
-- **Language :** [Python3](https://python.org)
-- **Library :** [Pyrogram v1.2.0](https://pyrogram.org)
-- **Server :** [Heroku](https://heroku.com)
-"""
+    about = "• **Bot :** YouTube DL \n\n• **Channel :** @EKBOTZ_UPDATE \n\n• **Language :** Python 3 \n\n• **Framework :** Pyrogram"
+
 await message.reply_text(about)
