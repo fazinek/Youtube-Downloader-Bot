653caf9e02a6da46acda5821c55da7bcb7ca4904
Mr Noob Developer <76585579+fazinek@users.noreply.github.com>
2021-06-15 14:28:15 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -2,6 +2,7 @@ from pyrogram import Client, Filters
 
 
 @Client.on_message(Filters.command(["help"]))
-async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
-    await message.reply_text(helptxt)
+async def help(client, message):
+    helptxt = f"Are You Confused With Me 🤣. \nJust Follow Up Below Instructions 😉.\n\n**Send Me A YouTube Video Link\nWait Bit For Analysis Your Link \nSelect Any Quality Button \nThen Select Upload Mode \nWait Few Seconds...I Will Upload It To Telegram."
+    helpbtn = InlineKeyboardMarkup([[InlineKeyboardButton("Cahnnel", url ="https://t.me/EKBOTZ_UPDATE")]])
+    await message.reply_text(helptxt), reply_markup = helpbtn
