8e776316215359c2647a69a7d1fde92cab4f001f
Mr Noob Developer <76585579+fazinek@users.noreply.github.com>
2021-06-15 18:44:36 +0530

    Create about.py

---
+++ plugins/about.py
@@ -0,0 +1,8 @@
+- **Bot :** `Rename Bot`
+- **Creator :** [Fayas](https://telegram.me/TheFayas)
+- **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
+- **Credits :** `Everyone in this journey`
+- **Source :** [Click here](https://github.com/FayasNoushad/Rename-Bot)
+- **Language :** [Python3](https://python.org)
+- **Library :** [Pyrogram v1.2.0](https://pyrogram.org)
+- **Server :** [Heroku](https://heroku.com)
