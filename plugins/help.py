b67595a44b3ec780ea06ecbf9f9a8e42874540fd
Mr Noob Developer <76585579+fazinek@users.noreply.github.com>
2021-06-16 10:32:02 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -5,4 +5,4 @@ from pyrogram import Client, Filters
 async def help(client, message):
     helptxt = f"Are You Confused With Me 🤣. \nJust Follow Up Below Instructions 😉.\n\n**Send Me A YouTube Video Link\nWait Bit For Analysis Your Link \nSelect Any Quality Button \nThen Select Upload Mode \nWait Few Seconds...I Will Upload It To Telegram.**"
     helpbtn = InlineKeyboardMarkup([[InlineKeyboardButton("Cahnnel", url ="https://t.me/EKBOTZ_UPDATE")]])
-    await message.reply_text(helptxt), reply_markup = helpbtn
+    await message.reply_text(helptxt, reply_markup = helpbtn)
